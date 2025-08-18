93b6f86f51b42b03698afbd19dff2ff2656a5607
James O'Sullivan <121300535+jamesdanedu@users.noreply.github.com>
2025-08-18 15:47:53 +0100

    Update appStore.js

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -1,9 +1,13 @@
 import { create } from 'zustand'
 
-export const useAppStore = create((set) => ({
+export const useAppStore = create((set, get) => ({
   currentTab: 'suggest',
   isOnline: true,
   
-  setCurrentTab: (tab) => set({ currentTab: tab }),
+  setCurrentTab: (tab) => {
+    console.log('AppStore: Setting tab from', get().currentTab, 'to', tab)
+    set({ currentTab: tab })
+    console.log('AppStore: Tab set to', get().currentTab)
+  },
   setOnlineStatus: (status) => set({ isOnline: status })
 }))
