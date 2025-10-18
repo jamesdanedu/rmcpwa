474fcb4fe883e2ce4a1e74dba6a993d57601031a
James O'Sullivan <121300535+jamesdanedu@users.noreply.github.com>
2025-10-18 16:41:15 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#FFD700" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="RMCBuddy" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
